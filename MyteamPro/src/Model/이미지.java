74c6033ce19835537465a5d482b43c47ac6f8de7
smhrd <smhrd@DESKTOP-6DGGVS8>
2022-09-03 17:35:08 +0900

    구현 완료ㅕ + 이미지

--- MyteamPro/src/Model/이미지.java
+++ MyteamPro/src/Model/이미지.java
@@ -3,131 +3,114 @@ package Model;
 public class 이미지 {
 
 	public static void main(String[] args) {
-		String pika = """
-   !..                     ..      \r
-   JBPJ^                ^7YP^      \r
-   :5B#B5^           .7PB##Y      .\r
-     ~YGBB?!J55555Y??GBBPJ~.:~7J5P!\r
-       .7GBBBBBBBBBBBBP!!?YPBBBBBB:\r
-        7BPGBBBBBBBBPGB5BBBBBBBBBY \r
-        JB^7YBBBBBGJ~!BPBBBBBGP5J: \r
-        JY55GBGGGGBGPP5JPBY^:.     \r
-        ~7?BBBGGGGGBG77!5BGJ:      \r
-         ?PGBBBBBBBBG5JGBGBGY:     \r
-        ^GBGGBGGGBBGGBB5P?^.       \r
-       ~GBBBBBPBBPGBBBBG7.         \r
-    . !BBBBBBBPGB5BBBBGGG^         \r
-    ?5PBPPBBBBPPPPBBBB5BBY?!       \r
-    .!YPG5PBBBPYYPBBBYPG5Y~.       \r
-      .!?!!?5Y!::?P577J?^          
-				""";
-		System.out.println(pika);
+		System.out.println("   !..                     ..      \\r\r\n"
+				+ "   JBPJ^                ^7YP^      \\r\r\n"
+				+ "   :5B#B5^           .7PB##Y      .\\r\r\n"
+				+ "     ~YGBB?!J55555Y??GBBPJ~.:~7J5P!\\r\r\n"
+				+ "       .7GBBBBBBBBBBBBP!!?YPBBBBBB:\\r\r\n"
+				+ "        7BPGBBBBBBBBPGB5BBBBBBBBBY \\r\r\n"
+				+ "        JB^7YBBBBBGJ~!BPBBBBBGP5J: \\r\r\n"
+				+ "        JY55GBGGGGBGPP5JPBY^:.     \\r\r\n"
+				+ "        ~7?BBBGGGGGBG77!5BGJ:      \\r\r\n"
+				+ "         ?PGBBBBBBBBG5JGBGBGY:     \\r\r\n"
+				+ "        ^GBGGBGGGBBGGBB5P?^.       \\r\r\n"
+				+ "       ~GBBBBBPBBPGBBBBG7.         \\r\r\n"
+				+ "    . !BBBBBBBPGB5BBBBGGG^         \\r\r\n"
+				+ "    ?5PBPPBBBBPPPPBBBB5BBY?!       \\r\r\n"
+				+ "    .!YPG5PBBBPYYPBBBYPG5Y~.       \\r\r\n"
+				+ "      .!?!!?5Y!::?P577J?^ ");
 	
-	String kkobug = """
-		
-           :/JJJ?!:                \r
-        :YGGGGGGGPGY~              \r
-        ~GGGGGP7?7GG5^             \r
-       75GGGGGY^^YGPJ7             \r
-      .JGGGGGGPPPGPYJ~             \r
-        ^J55PPP55YJJPJ             \r
-     :!??5P5YYYYYJYY57!:           \r
- .!7Y5YY5B#BBGGPPGGGJ?77.          \r
- :JJJJ?J###BB#BPPPPYJ5J7!          \r
-  .... ?###BB#G5YJJJP#777          \r
-       7BBBBBBBBG555BY~7~ .~?JYJ!. \r
-      ~PG##BB#BGP55557~~.!PGGGGGBJ \r
-     ^GPYYPPPPPPPGGGG5??5GGP5PPP5J.\r
-     75YJJ?~~7???GGGGYJY55YYJYJ?7: \r
-    .!77!~:      75PYJJ^.::^^^:    \r
-                 :7!?7!:           
-			""";
-	System.out.println(kkobug);
+	System.out.println("           :/JJJ?!:                \\r\r\n"
+			+ "        :YGGGGGGGPGY~              \\r\r\n"
+			+ "        ~GGGGGP7?7GG5^             \\r\r\n"
+			+ "       75GGGGGY^^YGPJ7             \\r\r\n"
+			+ "      .JGGGGGGPPPGPYJ~             \\r\r\n"
+			+ "        ^J55PPP55YJJPJ             \\r\r\n"
+			+ "     :!??5P5YYYYYJYY57!:           \\r\r\n"
+			+ " .!7Y5YY5B#BBGGPPGGGJ?77.          \\r\r\n"
+			+ " :JJJJ?J###BB#BPPPPYJ5J7!          \\r\r\n"
+			+ "  .... ?###BB#G5YJJJP#777          \\r\r\n"
+			+ "       7BBBBBBBBG555BY~7~ .~?JYJ!. \\r\r\n"
+			+ "      ~PG##BB#BGP55557~~.!PGGGGGBJ \\r\r\n"
+			+ "     ^GPYYPPPPPPPGGGG5??5GGP5PPP5J.\\r\r\n"
+			+ "     75YJJ?~~7???GGGGYJY55YYJYJ?7: \\r\r\n"
+			+ "    .!77!~:      75PYJJ^.::^^^:    \\r\r\n"
+			+ "                 :7!?7!:           ");
 	
-	String paili = """
-                                        \r
-          ^7?JJ?!.                      \r
-        .JP555Y55?:                     \r
-        :?YYYYYY7~7:                    \r
-        :!YYYYYY!^7!                    \r
-       .JYY5YYYYJ7?7          .:        \r
-        :!7?JJJ?77?:         .!J.       \r
-          ^7?JJ??J?!^:.      7JYY^      \r
-      .~!JYYYGGGPYY55YY??~  ?G5P#?      \r
-      :?J?7JB####B5YJJ77~:  !GBGP~      \r
-           ?#BBBB#B5YY^      :Y!:       \r
-          .?#####B#GY5Y~   .^JJ         \r
-         75YPBBB##B5YYYY77?J5Y:         \r
-        .55YJJ5PGGGY5YYYJJJ7~           \r
-        ~YYJ?7:.:^~^~JYJJ^              \r
-       .!7~:.        :7YY7.             \r
-                       .                \r
-                                        
-			""";
-	System.out.println(paili);
+	System.out.println("                                        \\r\r\n"
+			+ "          ^7?JJ?!.                      \\r\r\n"
+			+ "        .JP555Y55?:                     \\r\r\n"
+			+ "        :?YYYYYY7~7:                    \\r\r\n"
+			+ "        :!YYYYYY!^7!                    \\r\r\n"
+			+ "       .JYY5YYYYJ7?7          .:        \\r\r\n"
+			+ "        :!7?JJJ?77?:         .!J.       \\r\r\n"
+			+ "          ^7?JJ??J?!^:.      7JYY^      \\r\r\n"
+			+ "      .~!JYYYGGGPYY55YY??~  ?G5P#?      \\r\r\n"
+			+ "      :?J?7JB####B5YJJ77~:  !GBGP~      \\r\r\n"
+			+ "           ?#BBBB#B5YY^      :Y!:       \\r\r\n"
+			+ "          .?#####B#GY5Y~   .^JJ         \\r\r\n"
+			+ "         75YPBBB##B5YYYY77?J5Y:         \\r\r\n"
+			+ "        .55YJJ5PGGGY5YYYJJJ7~           \\r\r\n"
+			+ "        ~YYJ?7:.:^~^~JYJJ^              \\r\r\n"
+			+ "       .!7~:.        :7YY7.             \\r\r\n"
+			+ "                       .                \\r");
 	
-	String dandegi = """
+	
+	System.out.println("         /!                        \\r\r\n"
+			+ "       .5BB7                       \\r\r\n"
+			+ "       YBGBG~                      \\r\r\n"
+			+ "      JBBGBG5~                     \\r\r\n"
+			+ "     JBBBBGP5J7:                   \\r\r\n"
+			+ "    ?GPBBP5PG5PYJ!:                \\r\r\n"
+			+ "   .5PPPYPGGGP555PPY?~:            \\r\r\n"
+			+ "   ^BGBPGGP555555555PG?            \\r\r\n"
+			+ "   ^GB5GBBBG5Y555555Y^             \\r\r\n"
+			+ "    ~?JPPGGPY5555555!              \\r\r\n"
+			+ "      ^?YYYY555555555?:            \\r\r\n"
+			+ "       .!5P555PP5555555!.          \\r\r\n"
+			+ "         .!Y5Y5PPPPP55YJ5?:        \\r\r\n"
+			+ "            :^^!7?JYYJJJJJJJ~:.    \\r\r\n"
+			+ "                   :^!!77????J!    \\r\r\n"
+			+ "                        .....     ");
+	
+	
+	System.out.println("                                   \\r\r\n"
+			+ "               .                   \\r\r\n"
+			+ "            !5GGPY?:               \\r\r\n"
+			+ "          .J##BBG5BG: ^~!!~.       \\r\r\n"
+			+ "          :JY5YJJ?YPY5GGGGG57.     \\r\r\n"
+			+ "      .?5P5JPPPG5JY5PGGGGGP55J     \\r\r\n"
+			+ "      ~PY5GBBBBP5PG55GGGB#BPY5^    \\r\r\n"
+			+ "       .?PGBBBGBBBGPPGP55555Y5^    \\r\r\n"
+			+ "      ^GBB#BBG##BB##GBBG5555YY     \\r\r\n"
+			+ "     :#&#&&BP#&###BGB#BBG55Y5^     \\r\r\n"
+			+ "     !&&&&#PYPPGGP5GBBBGGYJPJ      \\r\r\n"
+			+ "     .JPGP5Y5Y?7???JPPPG5: ~!      \\r\r\n"
+			+ "        .^7!~^      ?5Y?^          \\r");
+	
+	
+	System.out.println("                7G!. ~?          \\r\r\n"
+			+ "                 .5#BGPBP          \\r\r\n"
+			+ "                 P##BB##G:         \\r\r\n"
+			+ "          :J?.   !GBPPBB5:         \\r\r\n"
+			+ "          .JGJ!!:JYJPY5Y:          \\r\r\n"
+			+ "        7Y7YPPPBBPBGBGGJ.          \\r\r\n"
+			+ "  ~77?7^^!::~~:^^:5BBBGPJ~:        \\r\r\n"
+			+ " :J: .~J7.         !GGGJ:~7Y7      \\r\r\n"
+			+ " ^J.   :JJ~       .~PGGP?: .?5~    \\r\r\n"
+			+ "  ?!    :JY?^.  .?B##B5Y55J:.PPY^  \\r\r\n"
+			+ "  .?7:   ^JYYJ7!P&####YJYY5P^7^55^ \\r\r\n"
+			+ "   .?5J!: :?YYY5#BBGBPJJJJ5Y.  .   \\r\r\n"
+			+ "     !YP5?: ^7?YBGPPYJJJY5?.       \\r\r\n"
+			+ "      .!JY7   ^PP!^^::.PP:         \\r\r\n"
+			+ "         .   ~GG^     ~G5.         \\r\r\n"
+			+ "            ~BG:     ~PY5Y!^^^.    \\r\r\n"
+			+ "          .?##GY.     . .!JJY?^    \\r\r\n"
+			+ "          YGJ?JY^                  \r\n"
+			+ "			\");");
 			
-         /!                        \r
-       .5BB7                       \r
-       YBGBG~                      \r
-      JBBGBG5~                     \r
-     JBBBBGP5J7:                   \r
-    ?GPBBP5PG5PYJ!:                \r
-   .5PPPYPGGGP555PPY?~:            \r
-   ^BGBPGGP555555555PG?            \r
-   ^GB5GBBBG5Y555555Y^             \r
-    ~?JPPGGPY5555555!              \r
-      ^?YYYY555555555?:            \r
-       .!5P555PP5555555!.          \r
-         .!Y5Y5PPPPP55YJ5?:        \r
-            :^^!7?JYYJJJJJJJ~:.    \r
-                   :^!!77????J!    \r
-                        .....      
-			""";
-	System.out.println(dandegi);
+  
 	
-	String naelumi = """
-			
-                                   \r
-               .                   \r
-            !5GGPY?:               \r
-          .J##BBG5BG: ^~!!~.       \r
-          :JY5YJJ?YPY5GGGGG57.     \r
-      .?5P5JPPPG5JY5PGGGGGP55J     \r
-      ~PY5GBBBBP5PG55GGGB#BPY5^    \r
-       .?PGBBBGBBBGPPGP55555Y5^    \r
-      ^GBB#BBG##BB##GBBG5555YY     \r
-     :#&#&&BP#&###BGB#BBG55Y5^     \r
-     !&&&&#PYPPGGP5GBBBGGYJPJ      \r
-     .JPGP5Y5Y?7???JPPPG5: ~!      \r
-        .^7!~^      ?5Y?^          \r
-                                   
-			""";
-	System.out.println(naelumi);
-	
-	String myucheu = """
-			
-                  7G!. ~?          \r
-                 .5#BGPBP          \r
-                 P##BB##G:         \r
-          :J?.   !GBPPBB5:         \r
-          .JGJ!!:JYJPY5Y:          \r
-        7Y7YPPPBBPBGBGGJ.          \r
-  ~77?7^^!::~~:^^:5BBBGPJ~:        \r
- :J: .~J7.         !GGGJ:~7Y7      \r
- ^J.   :JJ~       .~PGGP?: .?5~    \r
-  ?!    :JY?^.  .?B##B5Y55J:.PPY^  \r
-  .?7:   ^JYYJ7!P&####YJYY5P^7^55^ \r
-   .?5J!: :?YYY5#BBGBPJJJJ5Y.  .   \r
-     !YP5?: ^7?YBGPPYJJJY5?.       \r
-      .!JY7   ^PP!^^::.PP:         \r
-         .   ~GG^     ~G5.         \r
-            ~BG:     ~PY5Y!^^^.    \r
-          .?##GY.     . .!JJY?^    \r
-          YGJ?JY^                  
-			""";
-	System.out.println(myucheu);
 	
 	}
 
